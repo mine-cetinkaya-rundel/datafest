a9764189e7a59143157999a1c51726e1bf51df22
mine-cetinkaya-rundel <mine@stat.duke.edu>
2017-04-04 05:52:12 -0400

    pipe version of variable renaming

--- 04_get_judge_data.R
+++ 04_get_judge_data.R
@@ -18,19 +18,16 @@ judges <- gs_title(judges_name) %>%
 
 # Rename columns ----------------------------------------------------
 
-judges_col_names <- names(judges)
-
-judges_col_names <- str_replace(judges_col_names, "Email address", "email")
-judges_col_names <- str_replace(judges_col_names, "Your affiliation: .{1,}", "affiliation")
-judges_col_names <- str_replace(judges_col_names, "Your title:", "title")
-judges_col_names <- str_replace(judges_col_names, "Do you have a preference .{1,}", "preference")
-judges_col_names <- str_replace(judges_col_names, "Check if you agree", "photo")
-judges_col_names <- str_replace(judges_col_names, "\\:", "")
-judges_col_names <- str_replace_all(judges_col_names, "-", "")
-judges_col_names <- str_replace_all(judges_col_names, " ", "_")
-judges_col_names <- tolower(judges_col_names)
-
-names(judges) <- judges_col_names
+names(judges) <- names(judges) %>%  
+  str_replace("Email address", "email") %>%
+  str_replace("Your affiliation: .{1,}", "affiliation") %>%
+  str_replace("Your title:", "title") %>%
+  str_replace("Do you have a preference .{1,}", "preference") %>%
+  str_replace("Check if you agree", "photo") %>%
+  str_replace("\\:", "") %>%
+  str_replace_all("-", "") %>%
+  str_replace_all(" ", "_") %>%
+  tolower()
 
 # Assign role -------------------------------------------------------
 
